1137e25ae2ded26de4014410ba16667a51f79e84
pmh-only <pmhstudio.pmh@gmail.com>
2020-06-25 22:57:50 +0900

    Whoa, this is cool

---
+++ events/onMessage.js
@@ -0,0 +1,25 @@
+const Query = require('../classes/Query')
+
+/**
+ * @param {import('../classes/Client')} client
+ * @param {import('discord.js').Message} msg
+ */
+function onMessage (client, msg) {
+  const { prefix } = client.settings
+  const { author, content } = msg
+
+  if (author.bot) return
+  if (!content.startsWith(prefix)) return
+
+  const query = new Query(prefix, content)
+  const target = client.commands.find(
+    (command = { aliases: [] }) =>
+      typeof command === 'function' &&
+      command.aliases.includes(query.cmd)
+  )
+
+  if (!target) return
+  target(client, msg)
+}
+
+module.exports = onMessage
