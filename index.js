4d9f39475cce03c38856ff6fe8bde2d0143d7a64
pmh-only <pmhstudio.pmh@gmail.com>
2020-06-25 23:13:40 +0900

    Airbnb lint

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
-const Client = require('./classes/Client')
-const client = new Client()
+const Client = require('./classes/Client');
 
-const onReady = require('./events/onReady')
-const onMessage = require('./events/onMessage')
+const client = new Client();
 
-client.start()
-client.regist('ready', onReady)
-client.regist('message', onMessage)
+const onReady = require('./events/onReady');
+const onMessage = require('./events/onMessage');
+
+client.start();
+client.regist('ready', onReady);
+client.regist('message', onMessage);
