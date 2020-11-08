ab90c0de5120b756d1f7fa0bea687964469ce661
VINTO1819 <develretr0@gmail.com>
2020-11-08 18:34:59 +0900

    Remove Lagacy Code Files

--- src/index.js
+++
@@ -1,9 +0,0 @@
-const Client = require('./classes/Client')
-const client = new Client()
-
-const onReady = require('./events/onReady')
-const onMessage = require('./events/onMessage')
-
-client.start()
-client.regist('ready', onReady)
-client.regist('message', onMessage)
